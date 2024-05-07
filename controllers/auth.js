2ab172d3e12cafd95ccb08cfda50d2ec79a9097e
Thamindu Madhushan <punyawa-im20022@stu.kln.ac.lk>
2024-05-07 17:48:39 +0530

    Add bailer and driver role update logout function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,75 +1,3 @@
-// import bcrypt from "bcrypt";
-// import db from "../connect.js";
-
-// export const register = (req, res) => {
-//   const q = "SELECT * FROM users WHERE email = ?";
-
-//   db.query(q, [req.body.email], (err, data) => {
-//     if (err) return res.json(err);
-//     if (data.length) return res.status(409).json("User already exists!");
-
-//     const salt = bcrypt.genSaltSync(10);
-//     const hash = bcrypt.hashSync(req.body.password, salt);
-
-//     const insertQuery =
-//       "INSERT INTO users (`firstname`, `lastname`, `email`,`password`, `role`) VALUES (?)";
-//     const values = [
-//       req.body.firstname,
-//       req.body.lastname,
-//       req.body.email,
-//       hash,
-//       req.body.role,
-//     ];
-
-//     db.query(insertQuery, [values], (err, data) => {
-//       if (err) return res.json(err);
-//       return res.status(200).json("User has been created.");
-//     });
-//   });
-// };
-
-// export const login = (req, res) => {
-//   const sql = "SELECT * FROM users WHERE email = ?";
-//   db.query(sql, [req.body.email], (err, data) => {
-//     if (err) {
-//       return res.json({ error: "Error for fetching" });
-//     }
-//     if (data.length > 0) {
-//       bcrypt.compare(
-//         req.body.password.toString(),
-//         data[0].password,
-//         (err, response) => {
-//           if (err) {
-//             return res.json({ error: "password comparison error" });
-//           }
-         
-//           if (response) {
-//             req.session.user = {
-//               id: data[0].id,
-//               firstname: data[0].firstname,
-//               email: data[0].email
-//             };
-//             return res.json({ Login: true, user: req.session.user });
-//           }
-//           return res.json({ Login: false });
-//         }
-//       );
-//     } else {
-//       return res.json({ Login: false });
-//     }
-//   });
-// };
-
-// export const logout = (req, res) => {
-//   req.session.destroy((err) => {
-//     if (err) {
-//       return res.status(500).json({ error: "Logout failed" });
-//     }
-//     res.clearCookie("connect.sid");
-//     res.status(200).json("User has been logged out.");
-//   });
-// };
-
 import bcrypt from "bcrypt";
 import db from "../connect.js";
 
@@ -109,6 +37,14 @@ export const register = (req, res) => {
           break;
         case 'supplier':
           insertRoleSpecificQuery = `INSERT INTO suppliers user_id, firstname, lastname) VALUES (?, ?, ?)`;
+          roleSpecificValues = [userid, req.body.firstname, req.body.lastname,];
+          break;
+          case 'bailer':
+          insertRoleSpecificQuery = `INSERT INTO bailers user_id, firstname, lastname) VALUES (?, ?, ?)`;
+          roleSpecificValues = [userid, req.body.firstname, req.body.lastname,];
+          break;
+          case 'driver':
+          insertRoleSpecificQuery = `INSERT INTO drivers user_id, firstname, lastname) VALUES (?, ?, ?)`;
           roleSpecificValues = [userid, req.body.firstname, req.body.lastname,];
           break;
         default:
@@ -154,11 +90,20 @@ export const login = (req, res) => {
   });
 };
 
+
 export const logout = (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      return res.json({ error: "Logout failed" });
+      console.error('Logout error:', err);
+      return res.status(500).json({ error: "Logout failed" });
     }
-    res.json({ message: "Logout successful" });
+    // Clear the session cookie
+    res.clearCookie('connect.sid'); // Replace 'connect.sid' with your cookie name if different
+    
+    // Inspect response headers to verify if the cookie is cleared
+    console.log('Response Headers:', res.getHeaders());
+    
+    return res.json({ message: "Logout successful" });
   });
 };
+;
