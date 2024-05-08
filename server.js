462439d713614cb3cbf9ec9165664ed062e13225
Thamindu Madhushan <punyawa-im20022@stu.kln.ac.lk>
2024-05-09 01:00:17 +0530

    admin routes add

--- server.js
+++ server.js
@@ -6,50 +6,55 @@ import bodyParser from "body-parser";
 
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
-import productRoutes from './routes/product.js';
-import orderRoutes from './routes/order.js';
-import materialRoutes from './routes/materials.js';
-import employeeRoutes from './routes/employee.js';
-import customerRoutes from './routes/customer.js';
+import productRoutes from "./routes/product.js";
+import orderRoutes from "./routes/order.js";
+import materialRoutes from "./routes/materials.js";
+import employeeRoutes from "./routes/employee.js";
+import customerRoutes from "./routes/customer.js";
+import adminRoutes from "./routes/admin.js";
 
 const app = express();
 // ser
 app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:3000",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"]
-}));
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-app.use(session({
+app.use(
+  session({
     secret: "secretcode",
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 24 * 60 * 60 * 1000
-    }
-}));
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/materials', materialRoutes);
-app.use('/api/employees', employeeRoutes);
-app.use('/api/customer', customerRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/materials", materialRoutes);
+app.use("/api/employees", employeeRoutes);
+app.use("/api/customer", customerRoutes);
+app.use("/api/admin", adminRoutes);
 
 // Route to check if the user is authenticated
 app.get("/api/auth/authenticated", (req, res) => {
-    if(req.session.user) {
-        res.json({ authenticated: true, user: req.session.user });
-    } else {
-        res.json({ authenticated: false });
-    }
+  if (req.session.user) {
+    res.json({ authenticated: true, user: req.session.user });
+  } else {
+    res.json({ authenticated: false });
+  }
 });
 
 app.listen(3001, () => {
-    console.log("Server started on port 3001");
+  console.log("Server started on port 3001");
 });
-
