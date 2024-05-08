c4a298dc36ae0d3da4bd89dae242ee1e3829b3f2
Thamindu Madhushan <punyawa-im20022@stu.kln.ac.lk>
2024-05-09 01:05:19 +0530

    update accept order create cancel order

--- routes/order.js
+++ routes/order.js
@@ -1,36 +1,20 @@
-// import express from 'express';
-// import { getOrder, createOrder, deleteOrder, updateOrder, acceptOrder } from '../controllers/order.js';
-
-
-// const router = express.Router();
-
-// // Route to handle fetching all products
-// router.get('/id', getOrder);
-// // Route to handle adding a new product
-// router.post('/', createOrder);
-// // Route to handle deleting a product
-// router.delete('/:id', deleteOrder); // Define the delete route
-
-// router.put('/:id', updateOrder);
-
-// router.put('/accept/:id', acceptOrder);
-
-// export default router;
-
-
-// orderRoutes.js
-
-import express from 'express';
-import { getOrder, createOrder, deleteOrder, updateOrder, acceptOrder } from '../controllers/order.js';
+import express from "express";
+import {
+  getOrder,
+  createOrder,
+  deleteOrder,
+  updateOrder,
+  acceptOrder,
+  cancelOrder
+} from "../controllers/order.js";
 
 const router = express.Router();
 
-router.get('/:id', getOrder);
-router.post('/', createOrder);
-router.delete('/:id', deleteOrder);
-router.put('/:id', updateOrder);
-// router.put('/:id/accept', acceptOrder);
+router.get("/:id", getOrder);
+router.post("/", createOrder);
+router.delete("/:id", deleteOrder);
+router.put("/:id", updateOrder);
+router.put("/accept/:id", acceptOrder);
+router.put("/cancel/:id", cancelOrder);
 
 export default router;
-
-
