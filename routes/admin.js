ef239998ab243028ac8823a21bae848015f3f36c
Thamindu Madhushan <punyawa-im20022@stu.kln.ac.lk>
2024-06-05 13:36:59 +0530

    getBailingDetails, getAcceptBailingDetails, AcceptBailing, getCompletedOrders, getFinishedOrders routes added

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { getOrders, getAdmin, getAcceptedOrders, getCancelledOrders } from "../controllers/admin.js";
+import { getOrders, getAdmin, getAcceptedOrders, getCancelledOrders, getBailingDetails, getAcceptBailingDetails, AcceptBailing, getCompletedOrders, getFinishedOrders } from "../controllers/admin.js";
 
 const router = express.Router();
 
 router.get("/pending", getOrders);
 router.get("/accept", getAcceptedOrders);
 router.get("/cancelled", getCancelledOrders);
+router.get("/completed", getCompletedOrders);
+router.get("/finished", getFinishedOrders);
 router.get("/:id", getAdmin);
+router.get("/bailing/details", getBailingDetails);
+router.get("/bailing/details/accept", getAcceptBailingDetails);
+router.put("/bailing/details/accept", AcceptBailing);
+
 
 export default router;
