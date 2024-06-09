f0749a65e04ae4b0c43f6bed5af81462b709b96e
Thamindu Madhushan <punyawa-im20022@stu.kln.ac.lk>
2024-06-10 00:52:07 +0530

    allDriverCollection, allBailerCollection, getPaidSalary, updatePaymentStatus created getUnpaidSalary, getParameters updated

--- controllers/salary.js
+++ controllers/salary.js
@@ -52,8 +52,8 @@ const updateParameters = (req, res) => {
 });
 };
 
-const getSalary = (req, res) => {
-    const query = "SELECT * FROM salary";
+const getUnpaidSalary = (req, res) => {
+    const query = "SELECT salary.salary_id, admins.firstname AS admin_name, salary.basic_salary, salary.epf, salary.bonus, salary.full_payment, salary.total_quantity, salary.target_bonus, salary.status, salary.month, bailers.firstname AS bailer_name, drivers.firstname AS driver_name, DATE_FORMAT(salary.updated_at, '%Y-%m-%d') AS paid_date FROM salary LEFT JOIN admins ON admins.admin_id = salary.admin_id LEFT JOIN bailers ON bailers.user_id = salary.employee_id LEFT JOIN drivers ON drivers.user_id = salary.employee_id WHERE salary.status = 'unpaid'";
   
     // Execute the query
     db.query(query, (err, results) => {
@@ -96,21 +96,22 @@ const getSalary = (req, res) => {
       target_bonus, 
       full_payment, 
       total_quantity, 
-      status 
+      status,
+      month 
     } = req.body;
   
     // Insert the new parameters into the database
     const query = `
       INSERT INTO salary (
         employee_id, admin_id, basic_salary, epf, bonus, monthly_target, 
-        target_bonus, full_payment, total_quantity, status, unit_target_bonus
+        target_bonus, full_payment, total_quantity, status, unit_target_bonus, month
       ) 
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `;
   
     const values = [
       employee_id, admin_id, basic_salary, epf, bonus, monthly_target, 
-      target_bonus, full_payment, total_quantity, status, unit_target_bonus
+      target_bonus, full_payment, total_quantity, status, unit_target_bonus, month
     ];
   
     db.query(query, values, (err, result) => {
@@ -160,16 +161,59 @@ const getSalary = (req, res) => {
   //   });
   // };
 
+  // const driverCollection = (req, res) => {
+  //   const { startDate, endDate } = req.query;
+  //   const query = `
+  //     SELECT users.user_id, users.role, DATE_FORMAT(collections.updated_at, '%Y-%m-%d %H:%i:%s') AS date, collections.quantity, drivers.status 
+  //     FROM collections 
+  //     LEFT JOIN users ON collections.user_id  = users.user_id 
+  //     LEFT JOIN drivers ON users.user_id = drivers.user_id
+  //     WHERE role = 'driver' AND drivers.status = 'active' AND collections.updated_at BETWEEN ? AND ?
+  //   `;
+  
+  //   db.query(query, [startDate + " 00:00:00", endDate + " 23:59:59"], (err, results) => {
+  //     if (err) {
+  //       console.error("Error fetching salaries: ", err);
+  //       res.status(500).json({ error: "Error fetching salaries" });
+  //       return;
+  //     }
+  
+  //     res.json(results);
+  //   });
+  // };
+  
+  // const bailerCollection = (req, res) => {
+  //   const { startDate, endDate } = req.query;
+  //   const query = `
+  //     SELECT bd2.user_id, bd1.material_quantity AS quantity, DATE_FORMAT(bd1.updated_at, '%Y-%m-%d %H:%i:%s') AS date, users.role
+  //     FROM bailing_details AS bd1
+  //     LEFT JOIN bailers AS bd2 ON bd1.bailer_id = bd2.bailer_id
+  //     LEFT JOIN users ON bd2.user_id = users.user_id
+  //     WHERE bd1.status = 'accept' AND bailers.status = 'active' AND bd1.updated_at BETWEEN ? AND ?
+  //   `;
+  
+  //   db.query(query, [startDate + " 00:00:00", endDate + " 23:59:59"], (err, results) => {
+  //     if (err) {
+  //       console.error("Error fetching collections: ", err);
+  //       res.status(500).json({ error: "Error fetching collections" });
+  //       return;
+  //     }
+  
+  //     res.json(results);
+  //   });
+  // };
+  
   const driverCollection = (req, res) => {
     const { startDate, endDate } = req.query;
     const query = `
-      SELECT users.user_id, users.role, DATE_FORMAT(collections.updated_at, '%Y-%m-%d %H:%i:%s') AS date, collections.quantity 
-      FROM users 
-      LEFT JOIN collections ON users.user_id = collections.user_id 
-      WHERE role = 'driver' AND collections.updated_at BETWEEN ? AND ?
+      SELECT users.user_id, users.role, DATE_FORMAT(collections.updated_at, '%Y-%m-%d %H:%i:%s') AS date, collections.quantity, drivers.status 
+      FROM collections 
+      LEFT JOIN users ON collections.user_id  = users.user_id 
+      LEFT JOIN drivers ON users.user_id = drivers.user_id
+      WHERE role = 'driver' AND drivers.status = 'active' AND collections.updated_at BETWEEN ? AND ?
     `;
   
-    db.query(query, [startDate + " 00:00:00", endDate + " 23:59:59"], (err, results) => {
+    db.query(query, [`${startDate} 00:00:00`, `${endDate} 23:59:59`], (err, results) => {
       if (err) {
         console.error("Error fetching salaries: ", err);
         res.status(500).json({ error: "Error fetching salaries" });
@@ -187,10 +231,53 @@ const getSalary = (req, res) => {
       FROM bailing_details AS bd1
       LEFT JOIN bailers AS bd2 ON bd1.bailer_id = bd2.bailer_id
       LEFT JOIN users ON bd2.user_id = users.user_id
+      WHERE bd1.status = 'accept' AND bailers.status = 'active' AND bd1.updated_at BETWEEN ? AND ?
+    `;
+  
+    db.query(query, [`${startDate} 00:00:00`, `${endDate} 23:59:59`], (err, results) => {
+      if (err) {
+        console.error("Error fetching collections: ", err);
+        res.status(500).json({ error: "Error fetching collections" });
+        return;
+      }
+  
+      res.json(results);
+    });
+  };
+
+  
+  const allDriverCollection = (req, res) => {
+    const { startDate, endDate } = req.query;
+    const query = `
+      SELECT users.user_id, users.role, DATE_FORMAT(collections.updated_at, '%Y-%m-%d %H:%i:%s') AS date, collections.quantity, drivers.status 
+      FROM collections 
+      LEFT JOIN users ON collections.user_id  = users.user_id 
+      LEFT JOIN drivers ON users.user_id = drivers.user_id
+      WHERE role = 'driver' AND collections.updated_at BETWEEN ? AND ?
+    `;
+  
+    db.query(query, [`${startDate} 00:00:00`, `${endDate} 23:59:59`], (err, results) => {
+      if (err) {
+        console.error("Error fetching salaries: ", err);
+        res.status(500).json({ error: "Error fetching salaries" });
+        return;
+      }
+  
+      res.json(results);
+    });
+  };
+  
+  const allBailerCollection = (req, res) => {
+    const { startDate, endDate } = req.query;
+    const query = `
+      SELECT bd2.user_id, bd1.material_quantity AS quantity, DATE_FORMAT(bd1.updated_at, '%Y-%m-%d %H:%i:%s') AS date, users.role
+      FROM bailing_details AS bd1
+      LEFT JOIN bailers AS bd2 ON bd1.bailer_id = bd2.bailer_id
+      LEFT JOIN users ON bd2.user_id = users.user_id
       WHERE bd1.status = 'accept' AND bd1.updated_at BETWEEN ? AND ?
     `;
   
-    db.query(query, [startDate + " 00:00:00", endDate + " 23:59:59"], (err, results) => {
+    db.query(query, [`${startDate} 00:00:00`, `${endDate} 23:59:59`], (err, results) => {
       if (err) {
         console.error("Error fetching collections: ", err);
         res.status(500).json({ error: "Error fetching collections" });
@@ -201,8 +288,37 @@ const getSalary = (req, res) => {
     });
   };
   
+  const getPaidSalary = (req, res) => {
+    const query = "SELECT admins.firstname AS admin_name, salary.basic_salary, salary.epf, salary.bonus, salary.full_payment, salary.total_quantity, salary.target_bonus, salary.status, salary.month, bailers.firstname AS bailer_name, drivers.firstname AS driver_name, DATE_FORMAT(salary.updated_at, '%Y-%m-%d') AS paid_date FROM salary LEFT JOIN admins ON admins.admin_id = salary.admin_id LEFT JOIN bailers ON bailers.user_id = salary.employee_id LEFT JOIN drivers ON drivers.user_id = salary.employee_id WHERE salary.status = 'paid'";
   
+    // Execute the query
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error("Error fetching salaries: ", err);
+        res.status(500).json({ error: "Error fetching salaries" });
+        return;
+      }
+  
+      res.json(results);
+    });
+  };
+
+  const updatePaymentStatus = (req, res) => {
+    const salary_id = req.params.id;
+    const { status, adminId } = req.body;
+    const query = "UPDATE salary SET status = ?, admin_id = ? WHERE salary_id = ?";
+  
+    // Execute the query
+    db.query(query, [status, adminId, salary_id], (err, results) => {
+      if (err) {
+        console.error("Error updating payment status: ", err);
+        res.status(500).json({ error: "Error updating payment status" });
+        return;
+      }
+  
+      res.json({ message: "Payment status updated successfully" });
+    });
+  };
 
 
-
-export { getParameters, addParameters, updateParameters, getSalary, saveSalary, driverCollection, bailerCollection };
+export { getParameters, addParameters, updateParameters, getUnpaidSalary, saveSalary, driverCollection, bailerCollection, allDriverCollection, allBailerCollection, getPaidSalary, updatePaymentStatus };
--- routes/salary.js
+++ routes/salary.js
@@ -1,15 +1,19 @@
 import express from "express";
-import { getParameters, addParameters, updateParameters, getSalary, saveSalary, driverCollection, bailerCollection,   } from "../controllers/salary.js";
+import { getParameters, addParameters, updateParameters, saveSalary, driverCollection, bailerCollection, allDriverCollection, allBailerCollection, getPaidSalary, getUnpaidSalary, updatePaymentStatus } from "../controllers/salary.js";
 
 const router = express.Router();
 
 router.get("/parameters", getParameters);
 router.post("/parameters/add", addParameters);
 router.put("/parameters/update/:id", updateParameters);
-router.get("/get", getSalary);
+router.get("/get/unpaid", getUnpaidSalary);
 router.post("/save", saveSalary);
 router.get("/collection/driver", driverCollection);
 router.get("/collection/bailer", bailerCollection);
+router.get("/all/collection/driver", allDriverCollection);
+router.get("/all/collection/bailer", allBailerCollection);
+router.get("/get/paid", getPaidSalary);
+router.put("/pay/:id", updatePaymentStatus);
 
 
 export default router
