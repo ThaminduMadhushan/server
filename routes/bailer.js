490af8805a014df8fbf4924dea4548989531fb43
Thamindu Madhushan <punyawa-im20022@stu.kln.ac.lk>
2024-06-10 00:50:09 +0530

    getSalary, updateAbout, updateDetails created

--- routes/bailer.js
+++ routes/bailer.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getBailer, getJobs, completeOrder, getBailingDetails, createBailing, getAcceptBailingDetails, updateBailing } from "../controllers/bailer.js";
+import { getBailer, getJobs, completeOrder, getBailingDetails, createBailing, getAcceptBailingDetails, updateBailing, getSalary, updateAbout, updateDetails } from "../controllers/bailer.js";
 
 const router = express.Router();
 
@@ -10,6 +10,9 @@ router.get("/bailing/details/:id", getBailingDetails);
 router.post("/add", createBailing);
 router.get("/details/accept/:id", getAcceptBailingDetails);
 router.put("/details/update/:id", updateBailing);
+router.get("/salary/:id", getSalary);
+router.put("/edit/about/:id", updateAbout);
+router.put("/:id", updateDetails);
 
 
 export default router;
